a0521b2cf19419128a2323587f7566ba34bcb479
D7703_17 <d7703_17@d7703-17ui-imac.local>
2018-04-18 15:09:01 +0900

    picker

--- PickerView/ViewController.swift
+++ PickerView/ViewController.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+      
+      
 
+      @IBOutlet weak var myPV: UIPickerView!
+      @IBOutlet weak var myL: UILabel!
+      @IBOutlet weak var myL2: UILabel!
+      @IBOutlet weak var myL3: UILabel!
+      
       override func viewDidLoad() {
             super.viewDidLoad()
             // Do any additional setup after loading the view, typically from a nib.
+            //delegate 와 ViewController 연결
+            myPV.delegate = self
+            myPV.dataSource = self
       }
-
-      override func didReceiveMemoryWarning() {
-            super.didReceiveMemoryWarning()
-            // Dispose of any resources that can be recreated.
+      
+      //UIPickerViewDataSource method
+      func numberOfComponents(in pickerView: UIPickerView) -> Int {
+            return 3
       }
-
-
+      
+      func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+            if component == 0 {
+                  return 20
+            } else if component == 1{
+                  return 100
+            } else {
+                  return 300
+            }
+      }
+      
+      //UIPickerViewDelegate method
+      func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+            
+           let myString = "\(row), \(component)"
+            myL.text = myString
+            return myString
+      }
+    
 }
 
