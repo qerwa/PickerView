0ca97f8f5bf56789b56019ea9c2992eaacbe9327
D7703_17 <d7703_17@d7703-17ui-imac.local>
2018-04-18 15:37:28 +0900

    component row

--- PickerView/ViewController.swift
+++ PickerView/ViewController.swift
@@ -15,38 +15,52 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
       @IBOutlet weak var myPV: UIPickerView!
       @IBOutlet weak var myL: UILabel!
       @IBOutlet weak var myL2: UILabel!
-      @IBOutlet weak var myL3: UILabel!
       
+      var color = ["WHITE","RED","GREEN"]
+      var number = ["1","2","3","4"]
       override func viewDidLoad() {
             super.viewDidLoad()
             // Do any additional setup after loading the view, typically from a nib.
             //delegate 와 ViewController 연결
             myPV.delegate = self
             myPV.dataSource = self
+            myL.text = color[0]
+            myL2.text = number[0]
       }
       
       //UIPickerViewDataSource method
       func numberOfComponents(in pickerView: UIPickerView) -> Int {
-            return 3
+            return 2
       }
       
       func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
             if component == 0 {
-                  return 20
-            } else if component == 1{
-                  return 100
+                  return 3
             } else {
-                  return 300
+                  return 4
             }
       }
       
       //UIPickerViewDelegate method
       func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
             
-           let myString = "\(row), \(component)"
-            myL.text = myString
-            return myString
+            if component == 0 {
+                  return color[row]
+            } else {
+                  return number[row]
+            }
       }
-    
-} 
+      func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int){
+            if component == 0{
+                  myL.text = color[row]}
+            else{
+                  myL2.text = number[row]}
+            }
+
+            
+//           let myString = "\(row), \(component)"
+//            myL.text = myString
+//            return myString
+      }
+
 
